df9a570eb31ac49875b00ffa3f0104c82bb53376
Bartosz Piechowiak <piechowiaq@gmail.com>
2022-11-27 17:08:07 +0100

    Setting up enviroment

---
+++ resources/views/charts.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <div class="bg-white rounded-md border my-8 px-6 py-6 mx-40">
+        <div>
+            <h2 class="text-2xl font-semibold">Charts</h2>
+                <div>
+                    <canvas id="myChart"></canvas>
+                </div>
+        </div>
+        @push('scripts')
+            <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+            <script>
+                const ctx = document.getElementById('myChart');
+
+                new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            </script>
+        @endpush
+    </div>
+</x-app-layout>
---
+++ resources/views/layouts/app-layout.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel Cookbook') }}</title>
+
+    <script defer src="https://unpkg.com/alpinejs@3.10.2/dist/cdn.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- Styles -->
+    @stack('styles')
+    @livewireStyles
+
+
+    <style>
+        .content a {
+            color: blue;
+        }
+
+        .content ul {
+            list-style-type: disc;
+            margin-left: 10px;
+        }
+    </style>
+</head>
+
+<body class="font-sans antialiased text-gray-900">
+<div class="min-h-screen bg-gray-100">
+
+    <div class="bg-blue-600 text-white">
+        <nav class="container mx-auto px-4 py-4 space-x-6">
+            <a href="/" class="hover:text-gray-200">Home</a>
+            <a href="/charts" class="hover:text-gray-200">Charts</a>
+
+        </nav>
+    </div>
+
+    <!-- Page Content -->
+    <main class="container mx-auto px-4 py-4">
+        {{ $slot }}
+    </main>
+</div>
+
+<!-- Scripts -->
+@livewireScripts
+@stack('scripts')
+</body>
+
+</html>
